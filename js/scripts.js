dbb3fe0e114072a557064eaebe728f251df1f475
zetteztan <88260978+zetteztan@users.noreply.github.com>
2021-08-10 11:26:56 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - One Page Wonder v6.0.3 (https://startbootstrap.com/theme/one-page-wonder)
+* Start Bootstrap - Heroic Features v5.0.3 (https://startbootstrap.com/template/heroic-features)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
